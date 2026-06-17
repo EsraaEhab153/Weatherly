e47525be5db7128e7d9adb0d009944fd033d931c
Esraa Ehba <esraaehab530@gmail.com>
2026-06-17 12:06:53 -0700

    Initial Commit

---
+++ Weatherly/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Weatherly
+//
+//  Created by Esraa Ehab on 17/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Weatherly/WeatherlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherlyApp.swift
+//  Weatherly
+//
+//  Created by Esraa Ehab on 17/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct WeatherlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
