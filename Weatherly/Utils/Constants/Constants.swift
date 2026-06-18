6923ffd5d339458b6b169ad558d9269075d782c8
Esraa Ehba <esraaehab530@gmail.com>
2026-06-18 10:19:21 -0700

    finished weather netwok call

---
+++ Weatherly/Utils/Constants/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Weatherly
+//
+//  Created by Esraa Ehab on 18/06/2026.
+//
+
+import Foundation
+
+struct Constants {
+    static let apiKey = "1dd9b5888ca44ea68b3204403261706"
+    static let baseURL = "https://api.weatherapi.com/v1"
+}
