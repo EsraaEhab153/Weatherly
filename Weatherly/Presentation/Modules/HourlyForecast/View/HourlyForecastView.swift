cbae5d6f80d7d91825281af803f11dc4f5804f17
Esraa Ehba <esraaehab530@gmail.com>
2026-06-22 15:21:19 -0700

    finished the header section of the hourly forecast screen

---
+++ Weatherly/Presentation/Modules/HourlyForecast/View/HourlyForecastView.swift
@@ -0,0 +1,48 @@
+//
+//  HourlyForecast.swift
+//  Weatherly
+//
+//  Created by Esraa Ehab on 19/06/2026.
+//
+
+import SwiftUI
+
+struct HourlyForecastView: View {
+    let hours: [Hour]
+    
+    var currentTimeOfDay : TimeOfDay {
+        Date().timeOfDay
+    }
+    
+    var body: some View {
+        ZStack {
+            
+            
+            Image(currentTimeOfDay.backgroundImageName)
+                .resizable()
+                .ignoresSafeArea()
+            
+           HourlyForeCastHeader(hours: hours)
+            
+        }
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+
+#Preview {
+    let dummyCondition = Condition(
+        text: "Partly cloudy",
+        icon: "//cdn.weatherapi.com/weather/64x64/day/116.png", 
+        code: 1003
+    )
+    
+    let dummyHours = [
+        Hour(time: "2026-06-22 15:00", tempC: 15.0, condition: dummyCondition),
+        Hour(time: "2026-06-22 16:00", tempC: 14.0, condition: dummyCondition),
+        Hour(time: "2026-06-22 17:00", tempC: 13.0, condition: dummyCondition),
+        Hour(time: "2026-06-22 18:00", tempC: 12.0, condition: dummyCondition)
+    ]
+    
+    return HourlyForecastView(hours: dummyHours)
+}
